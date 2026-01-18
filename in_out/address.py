ab07e8aef2dbe6174a2e7d7f307b4fe815276f49
Prince Mann <princemann2007@email.com>
2026-01-18 16:38:34 +0530

    initial commit

---
+++ in_out/address.py
@@ -0,0 +1,1 @@
+print("H.No. 333 \nsec 44 \nknl")
---
+++ in_out/os.py
@@ -0,0 +1,7 @@
+import os
+list_directory = os.listdir()
+current_directory = os.getcwd()
+
+print("Current Directory : \n",current_directory)
+print("")
+print("Files in current directory :\n",list_directory)
