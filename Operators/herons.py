e067282d70687d017e87731263965ee4385a8f25
Prince-Mann94 <princemann2007@gmail.com>
2026-01-26 23:43:15 +0530

    Fix indentation and print statement in herons.py

--- Operators/herons.py
+++ Operators/herons.py
@@ -4,9 +4,10 @@ b = int(input("Enter second num : "))
 c = int(input("Enter third num : "))
 
 if a+b>c and b+c>a and a+c>b:
-s = (a+b+c)/2
-area = s*(s-a)*(s-b)*(s-c)
+  s = (a+b+c)/2
+  area = s*(s-a)*(s-b)*(s-c)
 
-print("Area of triangle using heron's formula : %.2f\n"% math.sqrt(area))
+  print("Area of triangle using heron's formula : %.2f\n"% math.sqrt(area))
 else:
-printf("Triangle is not valid
+  print("Triangle is not valid")
+
