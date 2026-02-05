63c4fa9b7f2773414a3582df87d2085faf3787ee
Prince-Mann94 <princemann2007@gmail.com>
2026-02-05 19:14:15 +0530

    Add factorial calculation using math module

---
+++ loops/fact.py
@@ -0,0 +1,12 @@
+import math
+num = int(input("Enter num to find fact : "))
+print("Factorial :",math.factorial(num))
+
+# Alternate method to find factorial
+
+# num = int(input("Enter num to find factorial : "))
+# fact = 1
+# for i in range(1,num+1):
+#     fact *= i
+
+# print("Factorial :",fact)
